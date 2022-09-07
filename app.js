429a491c1fb21d4d9f528e9f11b4febd183456bb
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-09-07 17:59:28 -0400

    intial commit/upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// sounds
+const kevin_lee=new Audio('sounds/kevin_lee.mp3');
+const sick=new Audio('sounds/sick.mp3');
+const tito=new Audio('sounds/tito.mp3')
+
+
+const sounds=[kevin_lee,sick,tito];
+
+const pauseAudio= ()=>{
+
+	sounds.map(sound=>{
+		sound.pause();
+	})
+}
+
+// 
+const button=document.querySelector('.one');
+
+button.addEventListener('click',()=>{
+	
+pauseAudio();
+	kevin_lee.play();
+
+})
+// 
+const button2=document.querySelector('.two');
+
+button2.addEventListener('click',()=>{
+pauseAudio();
+	sick.play();
+})
+
+// 
+const button3= document.querySelector('.three');
+button3.addEventListener('click',()=>{
+	pauseAudio();
+	tito.play();
+
+})
