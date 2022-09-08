e9bca4d7c14be06fe6465ccfe2e911882ad1ee4d
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-09-08 00:56:54 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ const kevin_lee=new Audio('sounds/kevin_lee.mp3');
 const sick=new Audio('sounds/sick.mp3');
 const tito=new Audio('sounds/tito.mp3')
 
-
 const sounds=[kevin_lee,sick,tito];
 
 const pauseAudio= ()=>{
 
 	sounds.map(sound=>{
 		sound.pause();
+		sound.currentTime=0;
 	})
 }
 
 // 
-const button=document.querySelector('.one');
-
-button.addEventListener('click',()=>{
+const button1=document.querySelector('.one');
+button1.addEventListener('click',()=>{
 	
 pauseAudio();
 	kevin_lee.play();
-
 })
 // 
 const button2=document.querySelector('.two');
-
 button2.addEventListener('click',()=>{
 pauseAudio();
 	sick.play();
@@ -35,5 +32,6 @@ const button3= document.querySelector('.three');
 button3.addEventListener('click',()=>{
 	pauseAudio();
 	tito.play();
-
 })
+
+
